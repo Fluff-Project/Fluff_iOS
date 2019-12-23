b17df694f7ae9e16d369a0d25a102ba8a5555058
ORANZINO <hupajupa61@gmail.com>
2019-12-24 01:27:58 +0900

    [Update] Login view

--- Fluff_iOS/Fluff_iOS/Sources/ViewControllers/LaunchVC.swift
+++ Fluff_iOS/Fluff_iOS/Sources/ViewControllers/LaunchVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class LaunchVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
