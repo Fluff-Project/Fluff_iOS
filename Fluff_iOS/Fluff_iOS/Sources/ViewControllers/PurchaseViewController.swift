5dfde943bc8fefbbef6691455357d81d1b442779
dongminyoon <ehdals8965@naver.com>
2019-12-25 08:38:11 +0900

    [FIX] Info.plist 수정

---
+++ Fluff_iOS/Fluff_iOS/Sources/ViewControllers/PurchaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PurchaseViewController.swift
+//  Fluff_iOS
+//
+//  Created by 윤동민 on 2019/12/25.
+//  Copyright © 2019 TaeJin Oh. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
