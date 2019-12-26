a876cfd70497b8f2e81dfdd4335973c029eb93de
ORANZINO <hupajupa61@gmail.com>
2019-12-27 03:01:49 +0900

    [UPDATE] 탭바 메인 구현

--- Fluff_iOS/Fluff_iOS/Sources/ViewControllers/HomeVC.swift
+++ Fluff_iOS/Fluff_iOS/Sources/ViewControllers/HomeVC.swift
@@ -14,49 +14,25 @@ class HomeVC: UIViewController {
     @IBOutlet weak var bannerCollectionView: UICollectionView!
     @IBOutlet weak var bannerPageControl: CHIPageControlJalapeno!
     @IBOutlet weak var todayCollectionView: UICollectionView!
-    
     @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var stockLabel: UILabel!
     
-    var bannerList = [Banner]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setBanner()
-        
+        bannerCollectionView.dataSource = self
         todayCollectionView.dataSource = self
-        todayCollectionView.delegate = self
+        bannerCollectionView.delegate = self
+       bannerCollectionView.showsHorizontalScrollIndicator = false
+        todayCollectionView.showsHorizontalScrollIndicator = false
+
+        
         // Do any additional setup after loading the view.
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
-extension HomeVC {
-    func setBanner() {
-        let banner1 = Banner(bannerName: "beccaMchaffieFzde6ITjkwUnsplash2")
-        let banner2 = Banner(bannerName: "beccaMchaffieFzde6ITjkwUnsplash2")
-        let banner3 = Banner(bannerName: "beccaMchaffieFzde6ITjkwUnsplash2")
-        let banner4 = Banner(bannerName: "beccaMchaffieFzde6ITjkwUnsplash2")
-        let banner5 = Banner(bannerName: "beccaMchaffieFzde6ITjkwUnsplash2")
-        
-        bannerList = [banner1, banner2, banner3, banner4, banner5]
-    }
-    
-    
-}
-
-extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource{
+extension HomeVC: UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
             return 4
@@ -64,29 +40,56 @@ extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
+        let bannerImgList: [UIImage] = [#imageLiteral(resourceName: "beccaMchaffieFzde6ITjkwUnsplash2"),#imageLiteral(resourceName: "beccaMchaffieFzde6ITjkwUnsplash2"),#imageLiteral(resourceName: "beccaMchaffieFzde6ITjkwUnsplash2"),#imageLiteral(resourceName: "beccaMchaffieFzde6ITjkwUnsplash2")]
+        
         let todayImgList: [UIImage] = [#imageLiteral(resourceName: "84"), #imageLiteral(resourceName: "416"), #imageLiteral(resourceName: "416"), #imageLiteral(resourceName: "84")]
         let todayProductList: [String] = ["빗살무늬 폴로 셔츠", "다홍꽃 주렁주렁 가디건", "빗살무늬 폴로 셔츠", "다홍꽃 주렁주렁 가디건"]
         
-        let todayCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodayCollectionViewCell", for: indexPath) as! TodayCollectionViewCell
+        switch collectionView {
+            
+        case self.todayCollectionView:
+            print("todayCollectionview")
+            let todayCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodayCollectionViewCell", for: indexPath) as! TodayCollectionViewCell
         todayCollectionViewCell.todayImage.image = todayImgList[indexPath.row]
-        todayCollectionViewCell.todayPriceLabel.text = todayProductList[indexPath.row]
-        
-        
-        let bannerCell: BannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
-        
-        
-        let banner = bannerList[indexPath.row]
-        bannerCell.image?.image = banner.bannerImg
-        return bannerCell
+        todayCollectionViewCell.todayProductLabel.text = todayProductList[indexPath.row]
+            return todayCollectionViewCell
+            
+        case self.bannerCollectionView:
+            print("bannerCollectionView")
+            let bannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+            
+            bannerCell.bannerImg.image = bannerImgList[indexPath.row]
+            return bannerCell
+            
+        default:
+        return UICollectionViewCell()
+        }
     }
     
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        
-        let x = scrollView.contentOffset.x
-        let w = scrollView.bounds.size.width
-        let currentPage = Int(ceil(x/w))
-        print(currentPage)
-        self.bannerPageControl.set(progress: currentPage, animated: true)
-        
+}
+
+extension HomeVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension HomeVC: UICollectionViewDelegate {
+
+func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    let page = Int(targetContentOffset.pointee.x / self.bannerCollectionView.frame.width)
+  self.bannerPageControl.set(progress: page, animated: true)
+}
 }
