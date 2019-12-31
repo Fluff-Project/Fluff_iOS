f280a69ba97a64181fef8998bb4bb256d4fd2780
dongminyoon <ehdals8965@naver.com>
2019-12-31 23:20:13 +0900

    [UPDATE] Info.plist 통일

---
+++ Fluff_iOS/Fluff_iOS/Sources/ViewControllers/FluvVC.swift
@@ -0,0 +1,30 @@
+//
+//  FluvViewController.swift
+//  Fluff_iOS
+//
+//  Created by 윤동민 on 2019/12/31.
+//  Copyright © 2019 TaeJin Oh. All rights reserved.
+//
+
+import UIKit
+
+class FluvVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
